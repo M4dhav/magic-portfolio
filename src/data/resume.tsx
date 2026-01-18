b9bb3a61d6ba43e63f819acf0a3284a3ada79e10
Madhav Gupta <madhav.gupta7674@gmail.com>
2026-01-18 20:27:17 +0530

    feat: temporarily remove blogs

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -106,7 +106,7 @@ export const DATA = {
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
-    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+    // { href: "/blog", icon: NotebookIcon, label: "Blog" },
   ],
   contact: {
     email: "madhav.gupta7674@gmail.com",
